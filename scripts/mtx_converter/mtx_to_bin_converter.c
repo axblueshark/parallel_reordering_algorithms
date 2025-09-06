c008e0c97851677d3c3c66dec755b90f06ea6869
Alexandr Tabara <alexandrtabara@gmail.com>
2025-09-06 14:52:16 +0200

    Update matrix converter

--- scripts/mtx_converter/mtx_to_bin_converter.c
+++ scripts/mtx_converter/mtx_to_bin_converter.c
@@ -6,25 +6,25 @@ static const char help[] = "Simple converter from .mtx to .bin format.\n";
 
 int main( int argc, char **argv )
 {
-    FILE *f;
-    int row_idx, col_idx, m, n, nnz;  // m, n, nnz - matrix dimensions and number of nonzeros, respectively
-    double val;                       // value at (row_idx, col_idx)
+    FILE *file;
+    PetscInt row_idx, col_idx, m, n, nnz;  // m, n, nnz - matrix dimensions and number of nonzeros, respectively
+    PetscScalar val;                       // value at (row_idx, col_idx)
     Mat A;                            // matrix to be constructed
     PetscViewer viewer;
     char line[512];
 
     PetscCall( PetscInitialize(&argc, &argv, NULL, help) );
 
-    // some arguments are missing
+    // if some arguments are missing
     if ( argc < 3 ) {
-        PetscPrintf(PETSC_COMM_WORLD, "Some arguments are missing, expected format: %s input.mtx output.bin\n", argv[0]);
+        PetscPrintf( PETSC_COMM_WORLD, "Some arguments are missing, expected format: %s input.mtx output.bin\n", argv[0] );
         PetscFinalize();
         return 1;
     }
 
     // open input .mtx file
-    f = fopen( argv[1], "r" );
-    if ( !f ) {
+    file = fopen( argv[1], "r" );
+    if ( !file ) {
         PetscPrintf( PETSC_COMM_WORLD, "Could not open file: %s\n", argv[1] );
         PetscFinalize();
         return 1;
@@ -32,9 +32,9 @@ int main( int argc, char **argv )
 
     // skip comments (headers beginning with %)
     do {
-        if ( !fgets(line, sizeof(line), f) ) {
+        if ( !fgets(line, sizeof(line), file) ) {
             PetscPrintf( PETSC_COMM_WORLD, "Invalid matrix market file.\n" );
-            fclose(f);
+            fclose( file );
             PetscFinalize();
             return 1;
         }
@@ -56,26 +56,24 @@ int main( int argc, char **argv )
     
     if ( strstr(input_filename, "_s_") ) {
         symmetric = 1;
-
         PetscCall( MatSetOption(A, MAT_SYMMETRIC, PETSC_TRUE) );
-        PetscPrintf(PETSC_COMM_WORLD, "Matrix marked as symmetric based on filename: %s\n", input_filename);
     }
 
     // read and insert entries
-    for ( int i = 0; i < nnz; i++ ) {
-        if ( fscanf(f, "%d %d %lf", &row_idx, &col_idx, &val) != 3 ) {
+    for ( PetscInt i = 0; i < nnz; i++ ) {
+        if ( fscanf(file, "%d %d %lf", &row_idx, &col_idx, &val) != 3 ) {
             PetscPrintf( PETSC_COMM_WORLD, "Failed to read value #%d\n", i + 1 );
             break;
         }
         PetscCall( MatSetValue(A, row_idx - 1, col_idx - 1, val, INSERT_VALUES) );
 
-        // ensure symmetric values if matrix is symmetric
+        // ensure adding symmetric values if matrix is symmetric
         if ( symmetric && row_idx != col_idx ) {
             PetscCall( MatSetValue(A, col_idx - 1, row_idx - 1, val, INSERT_VALUES) );
         }
     }
 
-    fclose(f);
+    fclose(file);
     PetscCall( MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY) );
     PetscCall( MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY) );
 
